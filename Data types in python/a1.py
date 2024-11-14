f3fe3cb95f694f0c53ea0cdace39864e1a1a429d
princepro046 <princepro046@gmail.com>
2024-11-14 20:21:09 +0500

    Completed Data Types In Python

---
+++ Data types in python/a1.py
@@ -0,0 +1,26 @@
+# name = input("Enter Your Name :")
+# print("Nice To meet you", name)
+
+#Changing String To Integer
+
+# num1 = int(input("Enter Your Number :"))
+# num2 = int(input("Enter Your Number :"))
+
+# print(num1+num2)
+
+# num1 = int(input("Enter Your Number :"))
+# num2 = int(input("Enter Your Number :"))
+
+# print(num1*num2)
+
+# num1 = int(input("Enter Your Number :"))
+# num2 = int(input("Enter Your Number :"))
+
+# print(num1/num2)
+
+# num1 = int(input("Enter Your Number :"))
+# num2 = int(input("Enter Your Number :"))
+
+# print(num1-num2)
+
+
---
+++ Data types in python/a2.py
@@ -0,0 +1,5 @@
+name = "Arham"
+print(name[0:5])
+
+quote = "Face Failtures to be a success man"
+print(name[0:4])
